704cdbad65d79b7fa21210e3fc7c2cc470006dc5
Alon Maltsev <alonmaltsev@gmail.com>
2025-01-26 23:19:20 +0200

    styles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import "./Footer.css"
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <p>Contact us: support@musicapp.com</p>
+      <p>Follow us on social media:</p>
+      <div className="social-links">
+        <a href="#facebook">Facebook</a> | <a href="#twitter">Twitter</a>
+      </div>
       <p>&copy; 2025 Music Web Site</p>
     </footer>
   );
