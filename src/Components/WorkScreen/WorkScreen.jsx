597b9f0974dbbc107fb93293087f21328f45ba20
Alon Maltsev <alonmaltsev@gmail.com>
2025-02-26 19:50:57 +0200

    workscreen

--- src/Components/WorkScreen/WorkScreen.jsx
+++ src/Components/WorkScreen/WorkScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./WorkScreen.css";
 import Releases from "../Releases/Releases";
 import About from "../About/About";
